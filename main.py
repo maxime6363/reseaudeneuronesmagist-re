528fb8e34aec96a728bff1cb6c9ded0ab4f751fb
p.l <pierre.lry31@gmail.com>
2025-11-13 12:14:27 +0100

    base class

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #VARIABLES#
+import numpy as np
+class Reseau2neurone :
+
+    def __init__(self):
+        None
+
+    def forward_propagation(self):
+        pass
+
+    def backward_propagation(self):
+        pass
+
+
