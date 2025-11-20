1f9251a98f0919eb998db88eab42988159b1612c
p.l <pierre.lry31@gmail.com>
2025-11-20 12:16:28 +0100

    initialisation de la classe

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #VARIABLES#
 import numpy as np
 class Reseau2neurone :
-
-    def __init__(self):
-        None
-
+    def __init__(self, nb_couche, neurones_couche, valeur_poids_init, taille_image): #vecteur
+        self.reseau = {}
+        for couche in range(0,nb_couche-1):
+            self.reseau[couche] = np.full((neurones_couche[couche+1],neurones_couche[couche]+1),valeur_poids_init)
+            self.reseau[couche][:,neurones_couche[couche]]=1
     def forward_propagation(self):
         pass
 
